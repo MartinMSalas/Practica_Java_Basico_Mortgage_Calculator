9479d6b40d3d0b4322decce6d2ce0ff88b86227b
MartinMSalas <ingmartinmsalas@gmail.com>
2022-08-02 20:33:42 -0300

    Adding examples

---
+++ src/Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+    private String name;
+    private int kgOfFood;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getKgOfFood() {
+        return kgOfFood;
+    }
+
+    public Animal(String name, int kgOfFood) {
+        this.name = name;
+        this.kgOfFood = kgOfFood;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        // Ejercicio Calculadora de Hipoteca
         //System.out.println("Calculadora de Pagos mensuales de hipoteca !");
 
-        var mortgageCalculator = new MortgageCalculator();
+        //var mortgageCalculator = new MortgageCalculator();
 
-        var mortageReport = new MortgageReport(mortgageCalculator);
+        //var mortageReport = new MortgageReport(mortgageCalculator);
 
-        mortageReport.printMonthlyPayments();
-        mortageReport.printMonthPayment();
+        //mortageReport.printMonthlyPayments();
+        //mortageReport.printMonthPayment();
+
+        // Ejercicio Scanner
+        /*
+        Scanner scanner = new Scanner(System.in);
+        String iniciales="";
+        System.out.print("Ingrese su nombre: ");
+        iniciales += scanner.nextLine().charAt(0);
+        System.out.print("Ingrese su apellido: ");
+        iniciales += scanner.nextLine().charAt(0);
+        System.out.println("Sus iniciales son: "+iniciales);
+
+         */
+
+        // Ejercicio funciones
+
+        //System.out.println(esDivisible(4,2));
+
+        //Ejercicio Comida animal
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Tell me the name of your dog: ");
+        String name = scanner.nextLine();
+        System.out.print("Tell me how much food she eat: ");
+        int kgOfFood = scanner.nextInt();
+        var freya = new Animal(name,kgOfFood);
+
+        System.out.println(freya.getName()+" necesito "+cantidadPaquetes(freya)+" paquetes para estar satisfecha");
+
 
         /*
         var textBox1 = new TextBox();
@@ -32,14 +61,30 @@ public class Main {
         */
     }
 
+    private static int cantidadPaquetes(Animal freya) {
+        float accumulator = 0;
+        int packageQuantity =0 ;
+        Scanner scanner = new Scanner(System.in);
+        while(accumulator< freya.getKgOfFood()) {
+            System.out.print("Insert the size of the new package: ");
+            accumulator += scanner.nextFloat();
+            packageQuantity++;
+        }
+        return packageQuantity;
+    }
 
 
 
 
-
+    /*
+    static public boolean esDivisible(int numerador,int denominador){
+        return (numerador%denominador==0);
+    }
+    */
+    /*
     static public void greetUser(String name){
         System.out.println("Hi "+name+" good to see you");
     }
-
+    */
 }
 
